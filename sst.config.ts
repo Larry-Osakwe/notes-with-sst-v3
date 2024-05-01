5aa3224c34b0b7ff940f1076afd8fd00764f6268
Larry Osakwe <larryosak@gmail.com>
2024-04-30 22:54:02 -0700

    add storage components

--- sst.config.ts
+++ sst.config.ts
@@ -14,8 +14,25 @@ export default $config({
       handler: "lib/lambda.handler",
     });
 
+    const table = new sst.aws.Dynamo("NotesSST3", {
+      fields: {
+        userId: "string",
+        noteId: "string"
+      },
+      primaryIndex: { hashKey: "userId", rangeKey: "noteId" }
+    });
+
+    const bucket = new sst.aws.Bucket("NotesBucketSST3", {
+      public: true,
+    });
+
     new sst.aws.Nextjs("NotesApp", {
       openNextVersion: "3.0.0-rc.15",
     });
+
+    return {
+      Table: table.name,
+      Bucket: bucket.name,
+    };
   },
 });
